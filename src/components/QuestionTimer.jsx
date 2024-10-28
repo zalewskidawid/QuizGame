a379ce36c96488ea4642aad8285c6c2563ad6ae0
DawidstudentKUL <73649090+zalewskidawid@users.noreply.github.com>
2024-10-28 15:31:05 +0100

    Creating TImer with useEffect and Callback

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+        clearInterval(interval)
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+}
+
+export default QuestionTimer;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions";
-import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from "./QuestionTimer";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  
+
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer =  useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  }, [])
 
-  if(quizIsComplete) {
-    return <div id="summary">
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
         <img src={quizCompleteImg} alt="Trophy icon" />
         <h2>Quiz Completed!</h2>
-    </div>
+      </div>
+    );
   }
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5);
@@ -28,6 +33,11 @@ function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        ></QuestionTimer>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
