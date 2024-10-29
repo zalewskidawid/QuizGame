e5eb37daedbedf8be6855319c69b1826c1ec5053
DawidstudentKUL <73649090+zalewskidawid@users.noreply.github.com>
2024-10-29 16:55:16 +0100

    Finish quiz with summary module

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question";
+import Summary from "./Summary";
 
 function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
@@ -27,10 +27,7 @@ function Quiz() {
 
 	if (quizIsComplete) {
 		return (
-			<div id="summary">
-				<img src={quizCompleteImg} alt="Trophy icon" />
-				<h2>Quiz Completed!</h2>
-			</div>
+			<Summary userAnswers={userAnswers}></Summary>
 		);
 	}
 
